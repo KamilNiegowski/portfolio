391bdc9f29fcd4867e74f25c0df487d377d4d9b6
KamilN <kamil.niegowski01@gmail.com>
2022-12-26 18:50:45 +0100

    second commit

--- resources/views/layout/navbar-item.blade.php
+++ resources/views/layout/navbar-item.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+<li>
+    <a href="{{ $href }}" class="text-xs font-medium text-dark py-2 flex hover:text-primary dark:text-white lg:inline-flex lg:ml-6 xl:ml-12">
+        {{ $slot }}
+    </a>
+</li>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -27,7 +27,7 @@
                     <nav
                         :class="!navbarOpen && 'hidden'"
                         id="navbarCollapse"
-                        class="absolute right-0 top-full bg-white py-5 px-6 z-50 shadow rounded-lg w-full dark:bg-slate-900 dark:text-gray-300 lg:px-0 lg:max-w-full lg:w-full lg:right-4 lg:block lg:static lg:shadow-none"
+                        class="absolute right-0 top-full bg-white py-5 px-6 z-50 shadow rounded-lg w-full dark:bg-[#000000] dark:text-white lg:px-0 lg:max-w-full lg:w-full lg:right-4 lg:block lg:static lg:shadow-none"
                     >
                         <ul class="block lg:flex lg:items-center">
                             @foreach($navigationItems as $item)
