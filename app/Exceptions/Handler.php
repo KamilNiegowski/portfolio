411b4016b0b8cfef7017f70d01b8686122896e55
Kamil Niegowski <kamil.niegowski01@gmail.com>
2022-12-31 23:25:23 +0100

    four commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,50 +1,72 @@
 <?php
 
-namespace App\Exceptions;
+    namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+    use Exception;
+    use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Response;
+    use Throwable;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    class Handler extends ExceptionHandler
     {
-        $this->reportable(function (Throwable $e) {
+        /**
+         * A list of exception types with their corresponding custom log levels.
+         *
+         * @var array<class-string<Throwable>, \Psr\Log\LogLevel::*>
+         */
+        protected $levels = [
             //
-        });
+        ];
+
+        /**
+         * A list of the exception types that are not reported.
+         *
+         * @var array<int, class-string<Throwable>>
+         */
+        protected $dontReport = [
+            //
+        ];
+
+        /**
+         * A list of the inputs that are never flashed to the session on validation exceptions.
+         *
+         * @var array<int, string>
+         */
+        protected $dontFlash = [
+            'current_password',
+            'password',
+            'password_confirmation',
+        ];
+
+        /**
+         * Register the exception handling callbacks for the application.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            $this->reportable( function ( Throwable $e ) {
+                //
+            } );
+        }
+
+        /**
+         * Render an exception into an HTTP response.
+         *
+         * @param Request $request
+         * @param Exception $exception
+         * @return Response
+         * @throws Throwable
+         */
+        public function render( $request, $exception )
+        {
+            if ( $this->isHttpException( $exception ) ) {
+                if ( $exception->getStatusCode() == 404 ) {
+                    return response()->view( 'errors.' . '404', [], 404 );
+                }
+            }
+
+            return parent::render( $request, $exception );
+        }
     }
-}
