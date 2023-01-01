9a4a17d51122089962b70129aca48d274e2f1835
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-01-01 22:54:17 +0100

    four commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,9 @@
 
     namespace App\Http\Controllers;
 
+    use App\Mail\ContactMail;
     use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Mail;
 
     class ContactController extends Controller
     {
@@ -15,7 +17,9 @@
 
             ] );
 
-            //Send email
+
+            Mail::to( 'kamil.niegowski01@gmail.com' )
+                ->send( new ContactMail( $validate[ 'name' ], $validate[ 'email' ], $validate[ 'message' ] ) );
             return [ 'success' => true ];
         }
     }
---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,64 @@
+<?php
+
+    namespace App\Mail;
+
+    use Illuminate\Bus\Queueable;
+    use Illuminate\Contracts\Queue\ShouldQueue;
+    use Illuminate\Mail\Mailable;
+    use Illuminate\Mail\Mailables\Content;
+    use Illuminate\Mail\Mailables\Envelope;
+    use Illuminate\Queue\SerializesModels;
+
+    class ContactMail extends Mailable
+    {
+        use Queueable, SerializesModels;
+
+        /**
+         * Create a new message instance.
+         *
+         * @return void
+         */
+        public function __construct( public string $name, public string $email, public string $body )
+        {
+            //
+        }
+
+        /**
+         * Get the message envelope.
+         *
+         * @return Envelope
+         */
+        public function envelope()
+        {
+            return new Envelope(
+                subject: 'Contact Mail',
+            );
+        }
+
+        public function build(): ContactMail
+        {
+            return $this->subject( 'Contact mail from my WebSite' )->replyTo( $this->email )->view( 'email.contact' );
+        }
+
+        /**
+         * Get the message content definition.
+         *
+         * @return Content
+         */
+        public function content()
+        {
+            return new Content(
+                view: 'email.contact',
+            );
+        }
+
+        /**
+         * Get the attachments for the message.
+         *
+         * @return array
+         */
+        public function attachments()
+        {
+            return [];
+        }
+    }
