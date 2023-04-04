b0d85f686e1e4c54316d465511d56fdaaa0ee8c9
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-04-04 20:51:54 +0200

    test test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,17 @@
 <?php
+    
+    namespace Tests\Unit;
+    
+    use PHPUnit\Framework\TestCase;
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
+    class ExampleTest extends TestCase
     {
-        $this->assertTrue(true);
+        /**
+         * A basic test example.
+         *
+         * @return void
+         */
+        public function test_that_true_is_true()
+        {
+        }
     }
-}
