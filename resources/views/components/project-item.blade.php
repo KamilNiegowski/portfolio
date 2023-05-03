02ea538fea27600af90920e06e1aaec7edff3223
KamilNIegowski <kamil@KamilN>
2023-05-03 11:34:34 +0200

    view cart project

--- resources/views/components/project-item.blade.php
+++ resources/views/components/project-item.blade.php
@@ -3,9 +3,9 @@
         {{--        :class="selectedTab === 'all' || categories.includes(selectedTab) ? 'block' : 'hidden'"--}}
         {{--        class="w-full md:w-1/2 xl:w-1/3 px-4"--}}
 >
-  <div class="relative mb-12">
+  <div class="relative mb-12 mx-2">
     <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
-      <img src="{{$project->thumbnail}}"
+      <img src="storage/{{$project->thumbnail}}"
            alt="{{$project->title}}"
            class="w-full h-[260px] object-cover"
       />
@@ -20,9 +20,8 @@
       <h3 class="font-bold text-lg text-dark dark:text-white mb-4">
         {{$project->title}}
       </h3>
-      <span class="text-center">dddddd</span>
-
       <x-button-link :href="$project['github']" target="_blank" variant="outline-primary">Zobacz więcej</x-button-link>
+      <p class="text-base text-body-color mt-5">{{$project->published_at}}</p>
     </div>
   </div>
 </div>
