8affdfc4c3f8f2ade2f8bcc8c2d1f9b2fadb5d5e
KamilNIegowski <kamil@KamilN>
2023-05-04 21:56:22 +0200

    optimalize website view

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,21 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="">
 <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Kamil Niegowski</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+  <meta name="robots" content="noindex, nofollow">
 
 
-    <!-- Fonts -->
-    <link href="{{Vite::asset('resources/css/app.css')}}" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="{{Vite::asset('resources/css/app.css')}}" rel="stylesheet">
 </head>
 <body class="antialiased text-gray-800 dark:text-white">
 <div class="min-h-screen bg-gray-100 dark:bg-slate-800 pt-24">
-    <x-layout.navbar></x-layout.navbar>
-    {{$slot}}
-    <x-layout.footer></x-layout.footer>
+  <x-layout.navbar></x-layout.navbar>
+  {{$slot}}
+  <x-layout.footer></x-layout.footer>
 </div>
 <script src="{{ Vite::asset('resources/js/app.js') }}"></script>
 </body>
