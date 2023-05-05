6dfaaee965c6e3f915c63556217f874141d6b5d1
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-05 23:57:29 +0200

    create dark mode file

---
+++ resources/js/dark_mode.js
@@ -0,0 +1,28 @@
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// ustaw motyw na podstawie wartości z localStorage (jeśli istnieje)
+if (localStorage.getItem('theme') === 'dark') {
+    themeToggleLightIcon.classList.remove('hidden');
+    document.documentElement.classList.add('dark');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+    document.documentElement.classList.remove('dark');
+}
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function () {
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // zmień motyw i zapisz preferencję do localStorage
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
