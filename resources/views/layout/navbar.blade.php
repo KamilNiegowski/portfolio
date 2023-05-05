1a56283fb4bb7c9115d926b34c41612610355c58
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-06 00:45:26 +0200

    create dark mode file

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -65,6 +65,37 @@
       </div>
     </div>
   </div>
-  {{--  @vite('resources/js/dark_mode.js')--}}
+  
+  <script>
+      const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+      const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+      // ustaw motyw na podstawie wartości z localStorage (jeśli istnieje)
+      if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+          themeToggleLightIcon.classList.remove('hidden');
+          document.documentElement.classList.add('dark');
+      } else {
+          themeToggleDarkIcon.classList.remove('hidden');
+          document.documentElement.classList.remove('dark');
+      }
+
+      const themeToggleBtn = document.getElementById('theme-toggle');
+
+      themeToggleBtn.addEventListener('click', function () {
+          // toggle icons inside button
+          themeToggleDarkIcon.classList.toggle('hidden');
+          themeToggleLightIcon.classList.toggle('hidden');
+
+          // zmień motyw i zapisz preferencję do localStorage
+          if (document.documentElement.classList.contains('dark')) {
+              document.documentElement.classList.remove('dark');
+              localStorage.setItem('theme', 'light');
+          } else {
+              document.documentElement.classList.add('dark');
+              localStorage.setItem('theme', 'dark');
+          }
+      });
+
+  </script>
 </header>
 <!-- ====== Navbar Section End -->
