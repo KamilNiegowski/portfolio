b1675b6f6bcf7382ac6e19b69d2acae415df2fb5
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-10 19:02:11 +0200

    improve view button project items

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,17 +1,17 @@
 <a
-    href="{{$href}}"
-    target="{{$target}}"
-    {{
-        $attributes->class([
-            'py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded leading-normal border inline-block transition',
-            'hover:bg-primary hover:border-primary hover:text-white' => $variant === 'outline-primary',
-            'hover:bg-red-700 hover:border-red-700 hover:text-white' => $variant === 'outline-red',
-            'bg-red-700 border-red-700 text-white hover:bg-red-800' => $variant === 'red',
-            'bg-gray-900 border-gray-700 text-white hover:bg-gray-800' => $variant === 'dark',
-            'bg-primary border-primary text-white hover:bg-opacity-80' => $variant === 'primary',
+        href="{{$href}}"
+        target="{{$target}}"
+        {{
+            $attributes->class([
+                'py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded leading-normal border inline-block transition',
+                'hover:bg-gray-500 hover:dark:bg-gray-700 hover:dark:border-gray-700 hover:text-white border-gray-500' => $variant === 'outline-primary',
+                'hover:bg-red-700 hover:border-red-700 hover:text-white' => $variant === 'outline-red',
+                'bg-red-700 border-red-700 text-white hover:bg-red-800' => $variant === 'red',
+                'bg-gray-900 border-gray-700 text-white hover:bg-gray-800' => $variant === 'dark',
+                'bg-primary border-primary text-white hover:bg-opacity-80' => $variant === 'primary',
 
-        ])
+            ])
 
-    }}>
-    {{$slot}}
+        }}>
+  {{$slot}}
 </a>
