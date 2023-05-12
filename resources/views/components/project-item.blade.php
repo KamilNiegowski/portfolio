92778211d4879a4bf05b9def1e52021ea873e97b
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-13 00:31:10 +0200

    reapir error 403 in login on the production

--- resources/views/components/project-item.blade.php
+++ resources/views/components/project-item.blade.php
@@ -3,13 +3,15 @@
         :class="selectedTab === 'all' || categories.includes(selectedTab) ? 'block' : 'hidden'"
         class="w-full md:w-1/2 xl:w-1/3 px-4"
 >
-  <div class="relative mb-12 mx-2">
-    <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
-      <img src="storage/{{$project->thumbnail}}"
-           alt="{{$project->title}}"
-           class="w-full h-[260px] object-cover"
-      />
-    </div>
+  <div class="relative mb-12">
+    <a href="{{$project->github}}" target="_blank">
+      <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+        <img src="storage/{{$project->thumbnail}}"
+             alt="{{$project->title}}"
+             class="w-full h-[260px] object-cover"
+        />
+      </div>
+    </a>
     <div
             class="text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]">
      <span class="text-sm text-primary font-semibold block mb-2 ">
---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,5 @@
+<div class="pr-4 w-60 max-w-full mb-6">
+  <a href="/" class="w-full flex items-center py-2">
+    <p class="text-2xl xl:text-2xl font-bold text-[#0aafff] dark:text-white">KamilNiegowski</p>
+  </a>
+</div>
