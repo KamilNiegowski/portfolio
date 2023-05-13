5b048a1ca9406610c846f4739f4a25d3cf9a0a59
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-13 14:08:27 +0200

    change view proejct item

--- resources/views/components/project-item.blade.php
+++ resources/views/components/project-item.blade.php
@@ -8,20 +8,25 @@
       <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
         <img src="storage/{{$project->thumbnail}}"
              alt="{{$project->title}}"
+             title="{{$project->title}}"
              class="w-full h-[260px] object-cover"
         />
       </div>
     </a>
-    <div
-            class="text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]">
+    <div class="text-center absolute bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]">
      <span class="text-sm text-primary font-semibold block mb-2 ">
          {{ implode(', ', $project->categories->pluck('title')->toArray()) }}
     </span>
-      <h3 class="font-bold text-lg text-dark dark:text-white mb-4">
+      <h3 class="font-bold text-lg text-dark mt-auto dark:text-white">
         {{$project->title}}
       </h3>
-      <x-button-link :href="$project['github']" target="_blank" variant="outline-primary">Zobacz więcej</x-button-link>
-      <p class="text-right text-[16px] text-body-color mt-5">Opublikowano: {{$project->getFormattedDate()}}</p>
+      <x-button-link :href="$project['github']"
+                     target="_blank"
+                     class="absolute bottom-14 left-10 right-10 "
+                     variant="outline-primary"> Zobacz więcej
+      </x-button-link>
+      <p class="absolute bottom-3 right-0 text-right text-[16px] text-body-color">
+        Opublikowano: {{$project->getFormattedDate()}}</p>
     </div>
   </div>
 </div>
