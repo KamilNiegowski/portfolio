f0560b6ddb2a708c7bb8b4accc3c7e810ae92f5e
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-14 19:10:34 +0200

    disable script Clarity, GSC and GA

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,31 +12,31 @@
   <meta name="robots" content="noindex, nofollow">
 
 
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SFJVK8KPDB"></script>
-  <script>
-      window.dataLayer = window.dataLayer || [];
+  {{--  <!-- Google tag (gtag.js) -->--}}
+  {{--  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SFJVK8KPDB"></script>--}}
+  {{--  <script>--}}
+  {{--      window.dataLayer = window.dataLayer || [];--}}
 
-      function gtag() {
-          dataLayer.push(arguments);
-      }
+  {{--      function gtag() {--}}
+  {{--          dataLayer.push(arguments);--}}
+  {{--      }--}}
 
-      gtag('js', new Date());
+  {{--      gtag('js', new Date());--}}
 
-      gtag('config', 'G-SFJVK8KPDB');
-  </script>
-  <script type="text/javascript">
-      (function (c, l, a, r, i, t, y) {
-          c[a] = c[a] || function () {
-              (c[a].q = c[a].q || []).push(arguments)
-          };
-          t = l.createElement(r);
-          t.async = 1;
-          t.src = "https://www.clarity.ms/tag/" + i;
-          y = l.getElementsByTagName(r)[0];
-          y.parentNode.insertBefore(t, y);
-      })(window, document, "clarity", "script", "gyunxlatpp");
-  </script>
+  {{--      gtag('config', 'G-SFJVK8KPDB');--}}
+  {{--  </script>--}}
+  {{--  <script type="text/javascript">--}}
+  {{--      (function (c, l, a, r, i, t, y) {--}}
+  {{--          c[a] = c[a] || function () {--}}
+  {{--              (c[a].q = c[a].q || []).push(arguments)--}}
+  {{--          };--}}
+  {{--          t = l.createElement(r);--}}
+  {{--          t.async = 1;--}}
+  {{--          t.src = "https://www.clarity.ms/tag/" + i;--}}
+  {{--          y = l.getElementsByTagName(r)[0];--}}
+  {{--          y.parentNode.insertBefore(t, y);--}}
+  {{--      })(window, document, "clarity", "script", "gyunxlatpp");--}}
+  {{--  </script>--}}
   <!-- Fonts -->
   @vite('resources/css/app.css')
 
