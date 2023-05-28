f371dcef6926c6020cd2a6ad04063db9c8dacba4
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-05-28 02:29:04 +0200

    create submenu

--- resources/views/layout/navbar-item.blade.php
+++ resources/views/layout/navbar-item.blade.php
@@ -1,10 +1,10 @@
-<li class="menu-item">
+<li class="menu-item ">
   <a href="{{ $menu->slug }}"
      class="text-xs font-medium text-dark py-2 flex hover:text-primary dark:text-white lg:inline-flex lg:ml-6 xl:ml-12">
     {{ $menu->menu_title }}
   </a>
   @if($menu->children->count() > 0)
-    <div class="submenu">
+    <div class="submenu bg-white dark:bg-slate-900 border border-[gray] p-4 z-9 position-absolute border-2 rounded-lg">
       <ul>
         @foreach($menu->children as $submenu)
           <li>
